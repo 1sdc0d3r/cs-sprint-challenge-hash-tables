57317ef0df0179a370dc4831670935efb0d06a0b
Braden <fish0859@gmail.com>
2020-06-04 16:50:28 -0600

    first test passed

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,16 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    cache = {}
+    for i in weights:
+        if i not in cache:
+            cache[i] = limit - i
+        else:
+            cache[f'{i+1}'] = limit - i
 
-    return None
+    for i in cache:
+        for j in cache:
+            if cache[i] + cache[j] == limit and i != j:
+                return(list(cache.keys()).index(j), list(cache.keys()).index(i))
+    else:
+        return None
