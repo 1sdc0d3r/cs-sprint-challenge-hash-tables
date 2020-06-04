615eee4e5ef850ddfc0253a0aa89d9540785d2e2
Braden <fish0859@gmail.com>
2020-06-04 17:39:11 -0600

    finished exercise 3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,17 +1,25 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    nums = {}
+    result = []
+    for arr in arrays:
+        for n in arr:
+            if n not in nums:
+                nums[n] = 1
+            else:
+                nums[n] += 1
+    for n in nums:
+        if nums[n] > 1:
+            result.append(n)
 
     return result
 
 
 if __name__ == "__main__":
-    arrays = []
+    arrays = [list(range(10, 20)), [1, 2, 3], [1, 2, 3]]
 
-    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+    #   arrays.appendlist(range(10, 20)), [1, 2, 3])
+    # arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+    # arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+    # arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
     print(intersection(arrays))
---
+++ hashtables/ex3/tempCodeRunnerFile.py
@@ -0,0 +1,19 @@
+   # result = intersection([
+        #     [1],
+        #     [1],
+        # ])
+        # self.assertTrue(result == [1])
+
+        # result = intersection([
+        #     [1, 2],
+        #     [1],
+        # ])
+        # self.assertTrue(result == [1])
+
+        # result = intersection([
+        #     [1, 2, 3],
+        #     [1, 4, 5, 3],
+        #     [1, 6, 7, 3]
+        # ])
+        # result.sort()
+        # self.assertTrue(result == [1, 3])
